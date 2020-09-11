e69bcb710f0b7864bfa054e4f5bd5aa212e19316
rqguzman <rafael.guzman@outlook.com>
2020-09-11 16:28:17 -0300

    Solucionada referencia ciclica entre Categoria e Produto

--- src/main/java/com/rafaelguzman/cursomc/services/CategoriaService.java
+++ src/main/java/com/rafaelguzman/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.rafaelguzman.cursomc.domain.Categoria;
 import com.rafaelguzman.cursomc.repositories.CategoriaRepository;
+import com.rafaelguzman.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }

1d29ee69e5b20c4c33378a2a7fea3e326a2744c8
rqguzman <rafael.guzman@outlook.com>
2020-09-20 12:27:40 -0300

    Disponibilizada paginacao com parametros opcionais para Categorias

--- src/main/java/com/rafaelguzman/cursomc/services/CategoriaService.java
+++ src/main/java/com/rafaelguzman/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rafaelguzman.cursomc.domain.Categoria;
@@ -36,7 +39,7 @@ public class CategoriaService {
 
 	public void delete(Integer id) {
 		find(id);
-		
+
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
@@ -46,5 +49,10 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }

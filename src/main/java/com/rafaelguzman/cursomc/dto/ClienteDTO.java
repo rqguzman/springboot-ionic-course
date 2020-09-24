32a9dcb3b76c873c25b6dbe1e0cf4386be0d9e4b
rqguzman <rafael.guzman@outlook.com>
2020-09-24 08:34:03 -0300

    Implementa validacao customizada email atualizacao Cliente

--- src/main/java/com/rafaelguzman/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/rafaelguzman/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.rafaelguzman.cursomc.domain.Cliente;
+import com.rafaelguzman.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;

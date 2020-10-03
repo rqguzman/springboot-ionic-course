45f57e1e35f9a0081a55cd183d264b12ad73e2a7
rqguzman <rafael.guzman@outlook.com>
2020-10-03 09:43:30 -0300

    Configurado endpoit para upload de arquivos

--- src/main/java/com/rafaelguzman/cursomc/CursomcApplication.java
+++ src/main/java/com/rafaelguzman/cursomc/CursomcApplication.java
@@ -1,17 +1,12 @@
 package com.rafaelguzman.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.rafaelguzman.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -19,8 +14,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		s3Service.uploadFile("/Users/rafaelguzman/Pictures/test_images/apple_web.png");
 		
 	}
 
--- src/main/java/com/rafaelguzman/cursomc/services/ClienteService.java
+++ src/main/java/com/rafaelguzman/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.rafaelguzman.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.rafaelguzman.cursomc.domain.Cidade;
 import com.rafaelguzman.cursomc.domain.Cliente;
@@ -38,6 +40,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public Cliente find(Integer id) {
 	
@@ -135,5 +140,9 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }

2e8affaac198a9d1e87f807ef7607d19655a20c3
rqguzman <rafael.guzman@outlook.com>
2020-10-03 08:36:12 -0300

    Implementada integracao com o Amazon S3

--- src/main/java/com/rafaelguzman/cursomc/CursomcApplication.java
+++ src/main/java/com/rafaelguzman/cursomc/CursomcApplication.java
@@ -1,13 +1,17 @@
 package com.rafaelguzman.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rafaelguzman.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -16,7 +20,8 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-
+		s3Service.uploadFile("/Users/rafaelguzman/Pictures/test_images/apple_web.png");
+		
 	}
 
 }
